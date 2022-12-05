27873dc4e1db7a3e339fa49e678d9dbf9596228c
Sergey Sultangaleev <sultangaleev.s@yandex.ru>
2022-12-05 10:12:28 +0700

    burger-controller

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.scss'
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
-import burgerMenu from "./js/modules/burgerMenu.js";
-
 window.Stimulus = Application.start()
 const context = require.context("./js/controllers", true, /\.js$/)
 Stimulus.load(definitionsFromContext(context))
-
-document.addEventListener('DOMContentLoaded', () => {
-    burgerMenu()
-})
